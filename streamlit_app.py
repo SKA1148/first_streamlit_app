63e64cfaee2a4655ad8b1a9018432d0b525954b3
SKA1148 <146942479+SKA1148@users.noreply.github.com>
2023-10-12 18:16:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,6 +27,6 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response7)
+streamlit.text(fruityvice_response)
 
 
